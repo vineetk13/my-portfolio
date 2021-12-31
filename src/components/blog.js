4c7341c8b185342fc2ff37cc0c10153538454498
vineetk13 <vineetkme.99@gmail.com>
2021-12-31 20:52:13 +0530

    blog card changes

--- src/components/blog.js
+++ src/components/blog.js
@@ -7,13 +7,9 @@ import CommentIcon from '../assets/illustrations/comment-regular.svg';
 import CommentDark from '../assets/illustrations/comment-dark.svg';
 
 const Container = styled.div`
-      // border:1px solid blue;
       font-family: 'Nunito', sans-serif;
-      // border: 1px solid #d0d0d0;
       background-color:#ffffff;
-      // width:70vw;
       max-width:350px;
-      // box-sizing:border-box;
       padding:15px 20px;
       box-shadow: 0 0 5px rgba(0,0,0,0.4);
       border-radius:5px;
@@ -44,27 +40,28 @@ const Title = styled.a`
       margin:0;
       padding:0;
       font-family: 'Poppins', sans-serif;
-      // font-weight:600;
       font-size:18px;
       text-decoration:none;
-      // border:1px solid blue;
-      width:calc(100% - 60px);
-      // line-height:2px;
-      // color:${props => props.theme=="dark" ? "#fffffe" : "#000000"};
       &:hover{
             text-decoration:underline;
             font-weight:900;
             cursor:pointer;
-            // border-bottom:2px solid black;
       }
 `; 
+const ReactionCount = styled.span`
+      font-size: 12px;
+`;
 const BottomSection = styled.div`
       display:flex;
-      // border:1px solid red;
-      // width:25%;
       align-items:center;
-      // justify-content:space-between;
-      color:${props => props.theme=="dark" ? "#fffffe" : "#192A56"};
+      justify-content:space-between;
+      & ${ReactionCount} {
+            color:${props => props.theme==="dark" ? "#fffffe" : "#192A56"};
+            font-weight: ${props => props.theme==="dark" ? 400 : 900};
+      }
+      & p{
+            color:${props => props.theme=="dark" ? "#94a1b2" : "black"};
+      }
       @media (max-width:760px){
             width:40%;
       }
@@ -72,9 +69,11 @@ const BottomSection = styled.div`
 const Date = styled.p`
       padding:0;
       margin:0;
-      width:55px;
+      // width:55px;
       // border:1px solid red;
+      font-size: 12px
 `;
+
 
 export const BlogComponent = (props) => {
       const { title, desc, date, reactions, link, comments, theme } = props;
@@ -83,14 +82,17 @@ export const BlogComponent = (props) => {
                   <div>
                         <TopSection theme={theme}>
                               <Title href={link} target="_blank">{title}</Title>
-                              <Date>{date}</Date>
+                             
                         </TopSection>
                         <Description>{desc}</Description>
                   </div>
                   <BottomSection theme={theme}>
-                        <FontAwesomeIcon style={{color:"#F0142F"}} icon={faHeart} />&ensp;{reactions}&emsp;
-                        <FontAwesomeIcon style={{color:theme=="dark" ? "#94a1b2" : "black"}} icon={faComment} />
-      {/*<img src={theme=="dark" ? CommentDark : CommentIcon} alt="comment icon" width="15px" height="15px" />*/}&ensp;{comments}
+                        <div>
+                              <FontAwesomeIcon style={{color:"#F0142F"}} icon={faHeart} />&ensp;<ReactionCount>{reactions}</ReactionCount>&emsp;
+                              <FontAwesomeIcon style={{color:theme=="dark" ? "#94a1b2" : "black"}} icon={faComment} />
+                              &ensp;<ReactionCount>{comments}</ReactionCount>
+                        </div>
+                        <Date>{date}</Date>
                   </BottomSection>
             </Container>
       )
