d47c9b338f15b06798f7f3e873e17f342f1c16ee
vineetk13 <vineetkme.99@gmail.com>
2025-01-31 09:57:09 +0530

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [react()],
   build: {
     outDir: 'build'
